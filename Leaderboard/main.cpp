43607cc42eca2fa9b73fef97850e3a4613363226
FaizNrsydn <faiznurrosyiddin2606@gmail.com>
2026-01-05 13:21:38 +0700

    fixxx

--- Leaderboard/main.cpp
+++ Leaderboard/main.cpp
@@ -1,5 +1,6 @@
 #include "leaderboard.h"
 #include <iostream>
+#include <limits>
 using namespace std;
 
 void clearScreen() {
@@ -8,139 +9,134 @@ void clearScreen() {
 #endif
 }
 
+void pause() {
+    cout << "\nTekan ENTER untuk melanjutkan...";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
+}
+
 int main() {
     BinTree tree;
     createTree(tree);
 
     int choice;
+
     do {
         clearScreen();
-        cout << "==========================================================================\n";
-        cout << "  |                        MENU LEADERBOARD BST                        |\n";
-        cout << "==========================================================================\n";
-        cout << "  | 1. Tambah Pemain                                                   |\n";
-        cout << "  | 2. Tampilkan Leaderboard (Score Tertinggi -> Terendah)             |\n";
-        cout << "  | 3. Cari Pemain berdasarkan ID                                      |\n";
-        cout << "  | 4. Cari Pemain berdasarkan Username                                |\n";
-        cout << "  | 5. Cari Pemain berdasarkan Score                                   |\n";
-        cout << "  | 6. Hapus Pemain berdasarkan ID                                     |\n";
-        cout << "  | 7. Hapus Pemain berdasarkan Username                               |\n";
-        cout << "  | 8. Update Score                                                    |\n";
-        cout << "  | 9. Tampilkan Jumlah Pemain                                         |\n";
-        cout << "  | 0. Keluar                                                          |\n";
-        cout << "==========================================================================\n";
-        cout << "Pilih menu: ";
+    cout << "==========================================================================\n";
+    cout << "  |                        MENU LEADERBOARD BST                        |\n";
+    cout << "==========================================================================\n";
+    cout << "  | 1. Tambah Pemain                                                   |\n";
+    cout << "  | 2. Tampilkan Leaderboard (Score Tertinggi -> Terendah)             |\n";
+    cout << "  | 3. Cari Pemain berdasarkan ID                                      |\n";
+    cout << "  | 4. Cari Pemain berdasarkan Username                                |\n";
+    cout << "  | 5. Cari Pemain berdasarkan Range Score                             |\n";
+    cout << "  | 6. Hapus Pemain berdasarkan ID                                     |\n";
+    cout << "  | 7. Hapus Pemain berdasarkan Username                               |\n";
+    cout << "  | 8. Update Score Pemain                                             |\n";
+    cout << "  | 9. Tampilkan Jumlah Pemain                                         |\n";
+    cout << "  | 0. Keluar                                                          |\n";
+    cout << "==========================================================================\n";
+    cout << "Pilih menu: ";
         cin >> choice;
-        cout << "==========================================================================\n";
-        cout << endl;
 
-        switch(choice) {
-            case 1:{
-                leaderboard p = inputPlayer(tree);
-                insertNode(tree, alokasi(p));
-                cout << "   >> Pemain berhasil ditambahkan!\n";
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            }
-            case 2:
-                printHeader();
-                printLeaderboard(tree);
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            case 3: {
-                string id;
-                cout << "==========================================================================" << endl;
-                cout << "Masukkan ID pemain yang dicari: ";
-                cin >> id;
-                printHeader();
-                searchById(tree,id);
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            }
-            case 4: {
-                string uname;
-                cout << "==========================================================================" << endl;
-                cout << "Masukkan Username pemain yang dicari: ";
-                cin >> uname;
-                printHeader();
-                searchByUsername(tree, uname);
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            }
-            case 5:{
-                int minScore, maxScore;
-                cout << "==========================================================================" << endl;
-                cout << "Masukkan Range score minimumnya: ";
-                cin >> minScore;
-                cout << "Masukkan Range score maximumnya: ";
-                cin >> maxScore;
-                printHeader();
-                searchByRange(tree, minScore, maxScore);
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            }
-            case 6:{
-                string idDelete;
-                cout << "==========================================================================" << endl;
-                cout << "Masukkan ID pemain yang ingin dihapus: ";
-                cin >> idDelete;
-                if (deleteById(tree, idDelete)) {
-                    cout << ">> Pemain dengan ID " << idDelete << " berhasil dihapus.\n";
-                }else{
-                    cout << ">> Pemain dengan ID " << idDelete << " tidak ditemukan.\n";
-                }
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            }
-            case 7:{
-                string UDelete;
-                cout << "==========================================================================" << endl;
-                cout << "Masukkan Username pemain yang ingin dihapus: ";
-                cin >> UDelete;
-                if (deleteByUsername(tree, UDelete)){
-                    cout << ">> Pemain dengan username "<< UDelete << " berhasil dihapus.\n";
-                }else{
-                    cout << ">> Pemain dengan username "<< UDelete << " tidak ditemukan.\n";
-                }
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            }
-            case 8:{
-                string usname;
-                int skorbaru;
-                cout << "==========================================================================" << endl;
-                cout << "Masukkan Username yang ingin di Update scorenya: ";
-                cin >> usname;
-                cout << "Masukkan score baru                            : ";
-                cin >> skorbaru;
-                if(updateScoreByUsername(tree, usname, skorbaru)){
-                    cout << "Score berhasil di Update" << endl;
-                }else{
-                    cout << "Score gagal di Update" << endl;
-                }
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            }
-            case 9:
-                cout << "==========================================================================" << endl;
-                cout << "Jumlah pemain: " << countPlayers(tree) << endl;
-                cout << "==========================================================================" << endl;
-                cout << endl;
-                break;
-            case 0:
-                cout << "Keluar...\n";
-                break;
-            default:
-                cout << "Pilihan tidak valid!\n";
-            }
-        } while (choice != 0);
+        clearScreen();
+
+        switch (choice) {
+
+        case 1: {
+            leaderboard p = inputPlayer(tree);
+            insertNode(tree, alokasi(p));
+            cout << ">> Pemain berhasil ditambahkan!\n";
+            break;
+        }
+
+        case 2:
+            printHeader();
+            printLeaderboard(tree);
+            break;
+
+        case 3: {
+            string id;
+            cout << "Masukkan ID pemain: ";
+            cin >> id;
+            printHeader();
+            searchById(tree, id);
+            break;
+        }
+
+        case 4: {
+            string uname;
+            cout << "Masukkan Username pemain: ";
+            cin >> uname;
+            printHeader();
+            searchByUsername(tree, uname);
+            break;
+        }
+
+        case 5: {
+            int minScore, maxScore;
+            cout << "Masukkan score minimum: ";
+            cin >> minScore;
+            cout << "Masukkan score maksimum: ";
+            cin >> maxScore;
+            printHeader();
+            searchByRange(tree, minScore, maxScore);
+            break;
+        }
+
+        case 6: {
+            string id;
+            cout << "Masukkan ID pemain yang ingin dihapus: ";
+            cin >> id;
+            if (deleteById(tree, id))
+                cout << ">> Pemain berhasil dihapus.\n";
+            else
+                cout << ">> Pemain tidak ditemukan.\n";
+            break;
+        }
+
+        case 7: {
+            string uname;
+            cout << "Masukkan Username pemain yang ingin dihapus: ";
+            cin >> uname;
+            if (deleteByUsername(tree, uname))
+                cout << ">> Pemain berhasil dihapus.\n";
+            else
+                cout << ">> Pemain tidak ditemukan.\n";
+            break;
+        }
+
+        case 8: {
+            string uname;
+            int score;
+            cout << "Masukkan Username: ";
+            cin >> uname;
+            cout << "Masukkan score baru: ";
+            cin >> score;
+            if (updateScoreByUsername(tree, uname, score))
+                cout << ">> Score berhasil diupdate.\n";
+            else
+                cout << ">> Gagal update score.\n";
+            break;
+        }
+
+        case 9:
+            cout << "Jumlah pemain saat ini: " << countPlayers(tree) << endl;
+            break;
+
+        case 0:
+            cout << "Terima kasih, program selesai.\n";
+            break;
+
+        default:
+            cout << "Pilihan tidak valid!\n";
+        }
+
+        if (choice != 0)
+            pause();
+
+    } while (choice != 0);
+
     return 0;
 }
